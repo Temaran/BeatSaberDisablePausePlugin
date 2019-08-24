312618f7de2d1e154474962d14352b5eea2e21c6
Temaran <temaran@gmail.com>
2019-08-24 19:10:43 +0200

    Seems to work all right

---
+++ README.md
@@ -0,0 +1,2 @@
+# Beat Saber SongBrowser
+Disables the pause button. You can still use the system button to trigger a pause though. Mawp.
---
+++ Source/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine.SceneManagement;
+using Harmony;
+using IPA;
+using System.Reflection;
+
+namespace DisablePausePlugin
+{
+    [HarmonyPatch(typeof(InstantPauseTrigger))]
+    [HarmonyPatch("Tick")]
+    public class InstantPauseTrigger_Tick_Patch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    public class Plugin : IBeatSaberPlugin
+    {
+        public const string VERSION_NUMBER = "1.0.0";
+        public static Plugin Instance;
+        public static IPA.Logging.Logger Log;
+        private static HarmonyInstance _harmony;
+
+        public void Init(object nullObject, IPA.Logging.Logger logger)
+        {
+            Log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+            Instance = this;
+
+            if (_harmony == null)
+            {
+                _harmony = HarmonyInstance.Create("com.github.harmony.disablepauseplugin");
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+        }
+
+        public void OnApplicationQuit()
+        {            
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        { 
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+        }
+    }
+}
